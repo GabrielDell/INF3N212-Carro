dbd48962dae31549e4b4c682b8dbcf8fb09a9e04
Gabriel Alano de Carvalho Dellamora <182120002@senacinformatica.br>
2023-03-29 21:48:32 -0300

    Atualização com o menu na main do código

--- src/inf3n212/carro/INF3N212Carro.java
+++ src/inf3n212/carro/INF3N212Carro.java
@@ -6,6 +6,8 @@ package inf3n212.carro;
 
 import controller.CCarro;
 import controller.CPessoa;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -13,7 +15,9 @@ import controller.CPessoa;
  */
 public class INF3N212Carro {
     public static CPessoa cadPessoa = new CPessoa();
-    public static CCarro cadCarro = new CCarro();    
+    public static CCarro cadCarro = new CCarro();
+    static Scanner leia = new Scanner(System.in);
+    
     /**
      * @param args the command line arguments
      */
@@ -25,5 +29,42 @@ public class INF3N212Carro {
      System.out.println(cadPessoa.getPessoas());
      System.out.println("---");
      System.out.println(cadCarro.getCarros());
-    } 
-}
+     
+    }//Fim do main
+    
+    public static int leiaNumInt(){
+        Scanner leiaNum = new Scanner(System.in);
+        try{
+            return leiaNum.nextInt();
+        }catch(InputMismatchException i){
+            System.out.println("Erro: "+i.getMessage()+ "\nTente novamente");
+            leiaNumInt();
+        }
+        return 99;
+    }//Fim da leiaNumInt()
+    public static void menu(){
+        System.out.println("--- Menu Principal ---");
+        System.out.println("1 - Ger.Pessoa");
+        System.out.println("2 - Ger.Carro");
+        System.out.println("0 - Sair");
+        System.out.println("Digite aqui: ");
+    }//Fim do menu()
+    public static void subMenu(int opM){
+        String subM = null;
+        if(opM == 1){
+            subM = "Pessoa";
+        }//Fim do primeiro if
+        if(opM == 2){
+            subM = "Carro";
+        }//Fim do segundo if
+        
+        System.out.println("-- Ger. " + subM + " --");
+        System.out.println("1 - Cadastrar" + subM);
+        System.out.println("2 - Editar" + subM);
+        System.out.println("3 - Listar" + subM + "s");
+        System.out.println("4 - Deletar" + subM);
+        System.out.println("0 - Voltar");
+        System.out.println("Digite aqui: ");
+        
+    }//Fim do subMenu(int opM)
+}//Fim da classe INF3N212Carro
